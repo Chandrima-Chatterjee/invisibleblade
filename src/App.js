a4b3d5f8834556cb32f871686bb1e37d2a6dbae7
Chandrima1098 <74922272+Chandrima-Chatterjee@users.noreply.github.com>
2026-07-22 19:32:42 -0600

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
